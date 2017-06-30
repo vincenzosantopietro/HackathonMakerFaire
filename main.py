e0e01afcfa629ac8414079791348f1689e53d9ce
Vincenzo Santopietro <vincenzo.santopietro@studenti.uniparthenope.it>
2017-06-30 20:09:31 +0200

    Added fake news

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # Copyright 2007 Google Inc.
 #
@@ -14,23 +15,42 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
 import webapp2
 import json
-
+from values import *
+from SoresaScraper import SoresaNewsScraper
 
 class MainHandler(webapp2.RequestHandler):
+
     def post(self):
         jsonobject = json.loads(self.request.body)
 
+        speech = ""
+        source = ""
+
+        if jsonobject['result']['metadata']['intentName'] == SORESA_NEWS_INTENT_NAME:
+            newsScraper = SoresaNewsScraper()
+            news_json = json.loads(newsScraper.getNews())
+
+            speech = "Ecco gli ultimi 3 articoli pubblicati sul sito soresa.it\n\n"
+            source = jsonobject['result']['source']
+
+            for i in range(3):
+                speech += news_json['result'][i]['date'] + ": " + news_json['result'][i]['title'] + " - link: " + news_json['result'][i]['url'] + "\n\n"
+
+
+
+
         self.response.headers['Content-Type'] = 'application/json'
 
-        self.response.write(
-            json.dumps({
-                "speech":"Meteo di " + jsonobject['result']['parameters']['location'],
-                "displayText":"ciao ciao",
-                "source":"example"
+        out_json = json.dumps({
+                "speech":speech,
+                "displayText":speech,
+                "source":source
             },indent=4)
-        )
+
+        self.response.write(out_json)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
---
+++ values.py
@@ -0,0 +1,3 @@
+# This Python file uses the following encoding: utf-8
+
+SORESA_NEWS_INTENT_NAME='SoresaNews'
