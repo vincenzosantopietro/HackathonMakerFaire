859882210dba3730b0077056b0b48c9cc69cf123
Emanuel <emanuel.dinardo@gmail.com>
2017-07-01 19:24:26 +0200

    Tante cose

--- user/handlers.py
+++ user/handlers.py
@@ -10,11 +10,11 @@ def insert_user(username, type, platform='telegram'):
     if user is None:
         user = AccountModel()
         user.username = username
-        user.key = user.get_key_from_username()
+        user.user_id = user.get_key_from_username()
         user.type = type
         user.put()
 
-        user_alias = AliasModel(parent=user.key)
+        user_alias = AliasModel(parent=user.user_id)
         user_alias.alias = username
         user_alias.platform = platform
         user_alias.put()
