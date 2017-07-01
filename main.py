c3a8fcfd1121c1f06db5f26224b34f003f02c243
vincenzosantopietro <xboxac@hotmail.it>
2017-07-01 19:50:44 +0200

    Merge branch 'master' of /Users/vincenzo/PycharmProjects/HackathonMakerFaire with conflicts.

--- main.py
+++ main.py
@@ -97,6 +97,7 @@ class MainHandler(webapp2.RequestHandler):
                      "Provincia: Provincia di Napoli\n" \
                      "Sito Web:  <a href='https://www.soresa.it/'>https://www.soresa.it/</a>"
 
+
         elif 'Contatti_Orari' in jsonobject['result']['metadata']['intentName']:
             office = jsonobject['result']['parameters']['ufficio'].lower()
             office = re.sub('[\s+]', '', office)
